ade53855074a5a80d7304301c143ca4cb50bee08
themxtr <maxxtreme200@gmail.com>
2026-02-16 20:22:47 +0530

    Updated CORS for production

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://your-frontend.vercel.app"
+    "https://auth-frontend-ten-iota.vercel.app/"
   ]
 }));
 
