f8a2d473a0fed68883b462fc5b87eecf73c4ed3f
themxtr <maxxtreme200@gmail.com>
2026-02-16 20:52:05 +0530

    Allow all origins temporarily

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://auth-frontend-ten-iota.vercel.app/"
-  ]
+  origin: "*" , 
 }));
 
 app.use(express.json());
